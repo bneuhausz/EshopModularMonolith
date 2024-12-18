82e9a3cc3335d92e0bd671524aee709495b75654
nb <bneuhausz@gmail.com>
2024-12-18 14:19:31 +0100

    added basics of shared module

---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog;
+
+public static class CatalogModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services
+        //    .AddApplicationServices()
+        //    .AddInfrastructureServices(configuration)
+        //    .AddApiServices();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+    {
+        //app
+        //    .UseApplicationServices()
+        //    .UseInfrastructureServices()
+        //    .UseApiServices();
+
+        return app;
+    }
+}
---
+++ src/Modules/Ordering/Ordering/OrderingModule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering;
+
+public static class OrderingModule
+{
+    public static IServiceCollection AddOrderingModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services
+        //    .AddApplicationServices()
+        //    .AddInfrastructureServices(configuration)
+        //    .AddApiServices();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseOrderingModule(this IApplicationBuilder app)
+    {
+        //app
+        //    .UseApplicationServices()
+        //    .UseInfrastructureServices()
+        //    .UseApiServices();
+
+        return app;
+    }
+}
