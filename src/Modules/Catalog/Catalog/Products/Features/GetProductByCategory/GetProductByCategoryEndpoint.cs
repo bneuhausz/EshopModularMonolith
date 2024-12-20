f8abfab629f05cde005f7554e4a76bc2c04792a2
nb <bneuhausz@gmail.com>
2024-12-20 20:41:22 +0100

    added carter endpoints for catalog features

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.Products.Features.GetProductByCategory;
+
+public record GetProductByCategoryResposne(IEnumerable<ProductDto> Products);
+
+public class GetProductByCategoryEndpoint
+    : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+            var response = result.Adapt<GetProductByCategoryResposne>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResposne>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category");
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.Products.Features.GetProducts;
+
+public record GetProductsResponse(IEnumerable<ProductDto> Products);
+
+public class GetProductsEndpoint
+    : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var respone = result.Adapt<GetProductsResponse>();
+            return Results.Ok(respone);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
